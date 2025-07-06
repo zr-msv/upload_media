237f302489f06e0e6dc2bdd52d8784898bd57698
ZM <zahra.m.7933@gmail.com>
2025-07-07 00:41:12 +0330

    route name fixed

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 use App\Http\Controllers\ProductMediaController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('products/{product}')->group(function () {
-Route::post('/media', [ProductMediaController::class, 'upload']);
-Route::get('/media', [ProductMediaController::class, 'list']);
-});
+Route::prefix('products/{product}')
+    ->as('products.')
+    ->group(function () {
+        Route::post('/media', [ProductMediaController::class, 'upload'])->name('media.upload');
+        Route::get('/media', [ProductMediaController::class, 'list'])->name('media.list');
+    });
 
-Route::delete('/media/{media}', [ProductMediaController::class, 'delete']);
+Route::delete('/media/{media}', [ProductMediaController::class, 'delete'])->name('media.delete');
