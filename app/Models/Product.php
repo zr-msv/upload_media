cbe266464c980977d58a8af388ba9a5afe2eb403
ZM <zahra.m.7933@gmail.com>
2025-07-07 00:34:46 +0330

    add factory and seeder for product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Zm\PolymorphicMedia\Traits\HasMedia;
 
 class Product extends Model
 {
-    use HasMedia;
+    use HasMedia,HasFactory;
 
     protected $fillable = ['name'];
 }
