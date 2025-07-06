6a4f93a70a76a33673328f8b16634c661ae17710
ZM <zahra.m.7933@gmail.com>
2025-07-06 23:45:37 +0330

    add url & extention to media model

--- packages/laravel-polymorphic-media/src/Models/Media.php
+++ packages/laravel-polymorphic-media/src/Models/Media.php
@@ -2,15 +2,38 @@
 
 namespace Zm\PolymorphicMedia\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\Storage;
 
 class Media extends Model
 {
-    protected $fillable = ['file_path'];
+    protected $fillable = [
+        'file_path',
+        'mediable_id',
+        'mediable_type',
+    ];
+
+    protected $appends = ['url', 'extension'];
 
     public function mediable(): MorphTo
     {
         return $this->morphTo();
     }
+
+    protected function url(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => asset(Storage::url($attributes['path']))
+        );
+    }
+
+    protected function extension(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => pathinfo($attributes['path'], PATHINFO_EXTENSION)
+        );
+    }
 }
+
